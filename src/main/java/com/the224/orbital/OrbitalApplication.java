648d51f84f4af85db4ce4e2a6e06737b95372e62
Marc-Andre <marc224andre@gmail.com>
2019-04-03 12:26:46 -0400

    [Add] Spring Quick Start

---
+++ src/main/java/com/the224/orbital/OrbitalApplication.java
@@ -0,0 +1,13 @@
+package com.the224.orbital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitalApplication.class, args);
+	}
+
+}

2879dd77079849e00c3f3977aa839dc8b2e6f2e1
Marc <224@live.ca>
2019-04-07 16:02:46 -0400

    [Add] Meta info

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Orbital
+# Orbital 
+
+Orbital is _a file Distributor_ who the main purpose is to be **Easy to use, easy to share**.
+
+## For now
+
+The project look more like... nothing. The goal is to add value in the future, that's why I choose to use spring boot as main framework.
+
+## To be done
+
+- ~~Easy to use~~
+- File permission
+    - Password Protection
+    - Domain Protection
+- Analytics Data
+
+
+
+
+
+
+
+
+
--- src/main/java/com/the224/orbital/controller/MainController.java
+++ src/main/java/com/the224/orbital/controller/MainController.java
@@ -3,16 +3,16 @@ package com.the224.orbital.controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = {"http://127.0.0.1:3000", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 public class MainController {
 
     public MainController() { }
 
-    @RequestMapping("/test")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+//    @RequestMapping("/test")
+//    public String index() {
+//        return "Greetings from Spring Boot!";
+//    }
 
 }
 
